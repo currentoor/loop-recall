e24c64100f7b65cd9a938c358f6157c5316a854d
Karan Toor <karanbirsinghtoor@gmail.com>
2015-10-14 21:53:39 -0700

    annotated model

--- server/loop_recall/app/models/card.rb
+++ server/loop_recall/app/models/card.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: cards
+#
+#  id         :integer          not null, primary key
+#  question   :text
+#  answer     :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Card < ActiveRecord::Base
 end
