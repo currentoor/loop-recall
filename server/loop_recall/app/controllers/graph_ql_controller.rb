0c8083e6fb140c71be2b0ac6c591f1a5154fd841
Karan Toor <karanbirsinghtoor@gmail.com>
2015-10-14 23:07:45 -0700

    able to query through graph_ql controller

--- server/loop_recall/app/controllers/graph_ql_controller.rb
+++ server/loop_recall/app/controllers/graph_ql_controller.rb
@@ -2,7 +2,10 @@ class GraphQlController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def query
-    binding.pry
+    query_string = params[:query]
+    query = GraphQL::Query.new(GraphSchema, query_string)
+
+    render transit: query.result
   end
 
   def mutation
--- server/loop_recall/config/routes.rb
+++ server/loop_recall/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'graph_ql/query'
-
-  get 'graph_ql/mutation'
+  post 'graph_ql/mutation'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
