966cd1c831be6ee35e7265eb02630bd055ef3a66
Karan Toor <karanbirsinghtoor@gmail.com>
2015-10-19 23:27:50 -0700

    got a simple poor man's relay component working for the study card

--- client/loop-recall/src/loop_recall/core.cljs
+++ client/loop-recall/src/loop_recall/core.cljs
@@ -15,32 +15,52 @@
   (let [previous (system-attr @conn :show-answer?)]
     (set-system-attrs! :show-answer? (not previous))))
 
-(let [q "query getCards { user(id: 1) { cards{id, question, answer} } }"]
-  (GET "http://localhost:3000/graph_ql/query"
-      {:params {:query (js/encodeURIComponent q)}
-       :response-format :transit
-       :handler (fn [resp]
-                  (inspect resp)
-                  )}))
+(def garib-relay-mixin
+  {:did-mount (fn [{[query] :rum/args :as state}]
+                (GET "http://localhost:3000/graph_ql/query"
+                    {:params          {:query (js/encodeURIComponent query)}
+                     :response-format :transit
+                     :handler         (fn [resp]
+                                        (swap! (:rum/local state)
+                                               assoc :is-loading? false
+                                               :data (resp "data")))})
+                state)})
 
-(defc study-card [db]
+(defcs garib-relay < (rum/local {:is-loading? true
+                                 :data        nil})
+                     garib-relay-mixin
+  [state query params children]
+  (let [{:keys [is-loading? data]} @(:rum/local state)]
+    (if is-loading?
+      [:div.center
+       (mui/circular-progress {:mode "indeterminate"})]
+      (children data))))
+
+(defn mod-shift [inc-dec i n]
+  (set-system-attrs! :show-answer? false)
+  (mod (+ inc-dec i) n))
+
+(def next-index     (partial mod-shift 1))
+(def previous-index (partial mod-shift -1))
+
+(defc study-card [db question answer subtitle & {:keys [prev next]}]
   [:div.row
-   [:div.col-md-6.col-md-offset-3
+   [:div.col-xs-12.col-sm-10.col-sm-offset-1
     (mui/card
-     (mui/card-title {:title "Card" :subtitle "1 out of 1"})
+     (mui/card-title {:title "Card" :subtitle subtitle})
 
-     (mui/card-text "Who framed Roger Rabbit?")
+     (mui/card-text question)
 
      [:div.row
-      [:div.col-md-2.center
+      [:div.col-xs-2.center
        (mui/icon-button
         {:iconClassName   "material-icons"
          :tooltipPosition "top-right"
          :tooltip         "Previous Card"
-         :onClick          toggle-answer}
+         :onClick          prev}
         "arrow_back")]
 
-      [:div.col-md-8.center
+      [:div.col-xs-8.center
        (if (system-attr db :show-answer?)
          (mui/card-actions
           (mui/raised-button {:onClick toggle-answer :backgroundColor "#820000" :primary true :label "Wrong"})
@@ -49,23 +69,34 @@
          (mui/card-actions
           (mui/flat-button {:onClick toggle-answer :label "Show Answer"})))]
 
-      [:div.col-md-2.center
+      [:div.col-xs-2.center
        (mui/icon-button
         {:iconClassName   "material-icons"
          :tooltipPosition "top-left"
          :tooltip         "Next Card"
-         :onClick         toggle-answer}
-        "arrow_forward")]
-      ]
+         :onClick         next}
+        "arrow_forward")]]
 
-     (mui/card-text "No idea!"))]])
+     (mui/card-text answer))]])
+
+(defcs study-deck [state db]
+  (garib-relay
+   "query getCards { user(id: 1) { cards{id, question, answer} } }" ""
+   (fn [data]
+     (let [cards                     (get-in data ["user" "cards"])
+           index                     (or (system-attr db :study/card-index) 0)
+           total                     (count cards)
+           {:strs [question answer]} (cards index)]
+       (study-card db question answer (str (inc index) " of " total)
+                   :prev #(set-system-attrs! :study/card-index (previous-index index total))
+                   :next #(set-system-attrs! :study/card-index (next-index index total)))))))
 
 (defc app < rum/reactive color-theme [conn]
   (let [db   (rum/react conn)
         page (system-attr db :page)]
     [:div
      (navbar)
-     (study-card db)
+     (study-deck db)
      ;; (condp = page
      ;;   (global-dash db)
      ;;   )
