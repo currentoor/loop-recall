73633415944152986d3f9db4677f78eae2ff1f44
Karan Toor <karanbirsinghtoor@gmail.com>
2015-12-07 22:06:00 -0800

    some minor tweaks

--- client/loop-recall/src/loop_recall/core.cljs
+++ client/loop-recall/src/loop_recall/core.cljs
@@ -25,23 +25,23 @@
         (mui/card-text
          "LoopRecall is an app designed to make the learning process more efficient.
           We use machine learning algorithms to implement custom intelligently spaced
-          repetition plans. Given a set of knowledge you would like review and retain,
-          consider the diagram below."
+          repetition plans. Given a collection of knowledge you would like review and
+          retain,consider the diagram below."
          [:ul
-          [:li "Create flash cards out of this knowledge."]
-          [:li "Put them in the first box on the left."]
+          [:li "Create flash cards based on the collection of knowledge."]
+          [:li "Put the cards in the first box on the left."]
           [:li "Review each card according to the label on the box."]
           [:li "Based on your response move the card forwards or backwards based on arrows."]
           [:li "Repeat."]]
          "That is basically it, except the boxes are implemented in software and the labels
-          (i.e. review intervals) on the boxes are custom generated based on your learning
+          (i.e. gap between reviews) on the boxes are custom generated based on your learning
           history.")
         [:img {:src "http://loop-recall-assets.s3-us-west-1.amazonaws.com/images/visual.svg"
                :width "100%" :height "100%"
                :alt "Spaced Repetition"}]
         (mui/card-text
          "If you like this app and feel charitable, "
-         [:a {:href "https://www.khanacademy.org/donate"} "here"]
+         [:a {:href "https://www.khanacademy.org/donate" :target "_blank"} "here"]
          " is a great cause that could use more funding.")
         (mui/card-text "- Karan")]])]]])
 
